4ade113b9152f5a855f62ceff05581f881881ab3
Michael Presley <mipresley23@gmail.com>
2022-06-20 17:43:42 -0400

    phase one complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Clock, { ClockToggle } from './components/Clock';
 import Folder from './components/Folder';
 import Weather from './components/Weather';
@@ -21,27 +21,35 @@ const folders = [
   { title: 'three', content: 'Third folder here' }
 ];
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showClock: true
-    };
-  }
-  
-  toggleClock = () => this.setState({ showClock: !this.state.showClock });
-  
-  render () {
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       showClock: true
+//     };
+//   }
+
+function App() {
+
+  const [ showClock, setShowClock] = useState(true);
+  const [toggleClock, setToggleClock] = useState(!showClock)
+
+
+  // toggleClock = () => this.setState({ showClock: !this.state.showClock });
+
+
+
+
     return (
       <div className="widgets">
         <Folder folders={folders} />
         <Weather />
-        <ClockToggle toggleClock={this.toggleClock} />
-        {this.state.showClock && <Clock />}
+        <ClockToggle toggleClock={toggleClock} />
+        {/* {this.state.showClock && <Clock />} */}
+        {showClock && <Clock />}
         <AutoComplete names={names} />
       </div>
     );
-  }
 }
 
-export default App;
+  export default App;
